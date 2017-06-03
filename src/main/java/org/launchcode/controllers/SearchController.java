313ded750f6b7d967a9f982de91c627e5d92b321
Christina McDonnell <christinamcdonnell@ymail.com>
2017-06-03 11:42:04 -0500

    Almost complete TechJobs-mvc.  still need to add fragments. I also changed the css file to make output match the class example.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
  * Created by LaunchCode
  */
 @Controller
-@RequestMapping("search")
+@RequestMapping(value = "search")
 public class SearchController {
 
     @RequestMapping(value = "")
@@ -23,5 +23,49 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
+        model.addAttribute("columns", ListController.columnChoices);
+        //ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
+
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "Jobs containing '" + searchTerm + "' in Any Column" );
+            model.addAttribute("jobs", jobs);
+        }
+        else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs containing '" + searchTerm + "' in " + searchType);
+            model.addAttribute("jobs", jobs);
+        }
+        return "search";
+    }
 }
+
+//   @RequestMapping(value = "jobs")
+//   public String listJobsByColumnAndValue(Model model,
+//                                          @RequestParam String column, @RequestParam String value) {
+//
+//        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
+//        model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
+//        model.addAttribute("jobs", jobs);
+//
+//        return "list-jobs";
+//    }
+//}
+
+/*
+ if (column.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+            return "list-jobs";
+        } else {
+            ArrayList<String> items = JobData.findAll(column);
+            model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
+            model.addAttribute("column", column);
+            model.addAttribute("items", items);
+            return "list-column";
+        }
+*/
