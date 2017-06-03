5c2191c5c8077536450e729f0446b940b176361a
Christina McDonnell <christinamcdonnell@ymail.com>
2017-06-03 18:03:14 -0500

    Almost complete TechJobs-mvc.  still need to add fragments. I also changed the css file to make output match the class example.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -42,30 +42,3 @@ public class SearchController {
         return "search";
     }
 }
-
-//   @RequestMapping(value = "jobs")
-//   public String listJobsByColumnAndValue(Model model,
-//                                          @RequestParam String column, @RequestParam String value) {
-//
-//        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
-//        model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
-//        model.addAttribute("jobs", jobs);
-//
-//        return "list-jobs";
-//    }
-//}
-
-/*
- if (column.equals("all")) {
-            ArrayList<HashMap<String, String>> jobs = JobData.findAll();
-            model.addAttribute("title", "All Jobs");
-            model.addAttribute("jobs", jobs);
-            return "list-jobs";
-        } else {
-            ArrayList<String> items = JobData.findAll(column);
-            model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
-            model.addAttribute("column", column);
-            model.addAttribute("items", items);
-            return "list-column";
-        }
-*/
