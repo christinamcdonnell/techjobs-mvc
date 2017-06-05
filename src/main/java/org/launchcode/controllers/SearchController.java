4e8dd8eab0f6e3ddcb1956e0ff82442a109d3901
Christina McDonnell <christinamcdonnell@ymail.com>
2017-06-05 18:52:34 -0500

    Almost complete TechJobs-mvc.  still need to add fragments. I also changed the css file to make output match the class example.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,7 +24,7 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
-    @RequestMapping(value = "results")
+    @RequestMapping(value = "", method = RequestMethod.POST)
     public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
         model.addAttribute("columns", ListController.columnChoices);
